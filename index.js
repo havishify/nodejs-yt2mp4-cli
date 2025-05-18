4ea60b6bcf6f2639a16e5d8d34b727df469de419
havishify <havish301@gmail.com>
2025-05-18 17:22:10 +0900

    Update logic

--- index.js
+++ index.js
@@ -11,130 +11,99 @@ function safeName(str = Date.now().toString()) {
   return str.replace(/[\\/:*?"<>|]/g, '_').trim() || 'video';
 }
 
-function run(cmd, args) {
+async function run(cmd, args) {
   console.log('\n', cmd, args.join(' '), '\n');
   return new Promise((res, rej) => {
     const ls = spawn(cmd, args, { stdio: 'inherit' });
     ls.on('error', rej);
-    ls.on('close', (code) => !code ? res() : rej(new Error(`${cmd} exited ${code}`)));
+    ls.on('close', (code) => code === 0 ? res() : rej(new Error(`${cmd} exited ${code}`)));
   });
 }
 
-async function getSingleVideo(args) {
-  const outputFile = `${safeName(args[0].title || Date.now().toString())}.mp4`;
-
-  const ls = [];
-  
-  for await (const { url, start, end } of args) {
-    const hasCut = Number.isFinite(start) && Number.isFinite(end);
-    const tmpFile  = path.join(tmpdir(), `${crypto.randomUUID()}.mp4`);
-    const outFile  = path.join(tmpdir(), `${crypto.randomUUID()}.mp4`);
-
-    await run('yt-dlp', [
+async function download(url, output) {
+  return new Promise((res) => {
+    run('yt-dlp', [
       '--no-warnings',
-      '-f', 'bestvideo+bestaudio',
+      '-f', 'bestvideo[height<=1080]+bestaudio/best[height<=1080]',
       '--merge-output-format', 'mp4',
+      '--remux-video', 'mp4',
+      '--recode-video', 'mp4',
       url,
-      '-o', hasCut ? tmpFile : outFile
-    ]);
-
-    if (hasCut) {
-      const dur = end - start;
-      await run('ffmpeg', [
-        '-hwaccel', 'cuda',
-        '-hwaccel_output_format', 'cuda',
-        '-ss', `${start}`,
-        '-i', tmpFile,
-        '-filter_complex', `[0:v]trim=0:end=${dur},setpts=PTS-STARTPTS[v];[0:a]atrim=start=0:end=${dur},asetpts=PTS-STARTPTS[a]`,
-        '-map', '[v]', '-map', '[a]',
-        '-c:v', 'h264_nvenc',
-        '-preset', 'p7',
-        '-cq', '23',
-        '-c:a', 'aac',
-        '-b:a', '192k',
-        outFile
-      ]);
-      await unlink(tmpFile);
-    }
-
-    ls.push(outFile);
-  }
-
-  const listfile = path.join(tmpdir(), `${crypto.randomUUID()}.txt`);
-  await writeFile(listfile, ls.map((v) => `file '${v}'`).join('\n'));
-
-  await run('ffmpeg', [
-    '-f', 'concat', '-safe', '0',
-    '-i', listfile,
-    '-c', 'copy',
-    outputFile
-  ]);
-
-  await unlink(listfile);
-  for await (const tmp of ls) {
-    await unlink(tmp);
-  }
-
-  console.log(`✔ ${outputFile} done`);
+      '-o', output
+    ]).then(res);
+  });
 }
 
-async function getVideo(title, url, start, end) {
-  const hasCut = Number.isFinite(start) && Number.isFinite(end);
-  const safeTitle = safeName(title);
-  const tmpFile  = path.join(tmpdir(), crypto.randomUUID() + '.mp4');
-  const outFile  = `${safeTitle}.mp4`;
+async function cut(tmp, start, end, output) {
+  const dur = end - start;
 
-  await run('yt-dlp', [
-    '--no-warnings',
-    '-f', 'bestvideo+bestaudio',
-    '--merge-output-format', 'mp4',
-    url,
-    '-o', hasCut ? tmpFile : outFile,
+  await run('ffmpeg', [
+    '-hwaccel', 'cuda',
+    '-hwaccel_output_format', 'cuda',
+    '-ss', `${start}`,
+    '-i', tmp,
+    '-filter_complex', `[0:v]trim=0:end=${dur},setpts=PTS-STARTPTS[v];[0:a]atrim=start=0:end=${dur},asetpts=PTS-STARTPTS[a]`,
+    '-map', '[v]', '-map', '[a]',
+    '-c:v', 'h264_nvenc',
+    '-preset', 'p7',
+    '-cq', '23',
+    '-c:a', 'aac',
+    '-b:a', '192k',
+    output
   ]);
 
-  if (hasCut) {
-    const dur = end - start;
-    await run('ffmpeg', [
-      '-hwaccel', 'cuda',
-      '-hwaccel_output_format', 'cuda',
-      '-ss', `${start}`,
-      '-i', tmpFile,
-      '-filter_complex', `[0:v]trim=0:end=${dur},setpts=PTS-STARTPTS[v];[0:a]atrim=start=0:end=${dur},asetpts=PTS-STARTPTS[a]`,
-      '-map', '[v]', '-map', '[a]',
-      '-c:v', 'h264_nvenc',
-      '-preset', 'p7',
-      '-cq', '23',
-      '-c:a', 'aac',
-      '-b:a', '192k',
-      outFile
-    ]);
-    await unlink(tmpFile);
-  }
+  await unlink(tmp);
+}
 
-  console.log(`✔ ${outFile} done`);
+async function dlandcut(url, input, output, start = -1, end = -1) {
+  const hasCut = end === -1;
+  await download(url, hasCut ? input : output);
+  if (hasCut) await cut(input, start, end, output);
 }
 
 (async () => {
   const options = [ '-one' ];
-  const goCombine = process.argv.slice(2).filter((v) => v.includes(options)).length !== 0;
-  const args = process.argv.slice(2).filter((v) => !v.includes(options)).map((v) => {
+  const goCombine = process.argv.slice(2).filter((v) => options.includes(v)).length !== 0;
+  const args = process.argv.slice(2).filter((v) => !options.includes(v)).map((v) => {
     try { return JSON.parse(v); }
-    catch { return { title: undefined, url: v, start: undefined, end: undefined }; }
+    catch { return { title: '', url: v, start: -1, end: -1 }; }
   });
 
   if (!args.length) {
     console.log(`Usage:
 yt2mp4 <ARG> [<ARG> ...]
-
-<ARG> = URL | "{\"title\":\"<FileName>\",\"url\":\"<URL>\",\"start\":<SEC>,\"end\":<SEC>}"`);
+  - <ARG> = URL | "{\"title\":\"<FileName>\",\"url\":\"<URL>\",\"start\":<SEC>,\"end\":<SEC>}"`);
     process.exit(1);
   }
 
   if (goCombine && args.length > 1) {
-    await getSingleVideo(args);
+    const outputFile = `${safeName(args[0].title || Date.now().toString())}.mp4`;
+    const ls = [];
+    
+    for await (const { url, start, end } of args) {
+      const output = path.join(tmpdir(), `${crypto.randomUUID()}.mp4`);
+      await dlandcut(url, path.join(tmpdir(), `${crypto.randomUUID()}.mp4`), output, start, end);
+      ls.push(output);
+    }
+
+    const listfile = path.join(tmpdir(), `${crypto.randomUUID()}.txt`);
+    await writeFile(listfile, ls.map((v) => `file '${v}'`).join('\n'));
+
+    await run('ffmpeg', [
+      '-f', 'concat', '-safe', '0',
+      '-i', listfile,
+      '-c', 'copy',
+      outputFile
+    ]);
+
+    await unlink(listfile);
+    for await (const tmp of ls) {
+      await unlink(tmp);
+    }
   } else {
-    for await (const { title, url, start, end } of args) {
-      await getVideo(title, url, start, end);
+    for await (const task of args) {
+      const { title, url, start, end } = task;
+      await dlandcut(url, path.join(tmpdir(), crypto.randomUUID() + '.mp4'), `${safeName(title)}.mp4`, start, end);
     }
   }
 
